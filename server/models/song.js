eada0936a07d0a205a12a4c61d486b70cf883cdd
Carlos Santos <cadu.vs@hotmail.com>
2024-11-23 10:37:54 -0300

    refactor: songs table no longer uses timestamps

--- server/models/song.js
+++ server/models/song.js
@@ -1,32 +1,36 @@
 const sequelize = require("../database");
 const { DataTypes } = require("sequelize");
 
-const Song = sequelize.define("songs", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Song = sequelize.define(
+  "songs",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album_cover: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  year: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  artist: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  album: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  album_cover: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  { timestamps: false }
+);
 
 module.exports = Song;
